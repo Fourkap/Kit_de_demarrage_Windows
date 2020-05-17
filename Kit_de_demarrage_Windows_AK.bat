3ea4b9645a70eaff246014700fc1cda0c8222f02
Alexandre Kaprielian <alex.kaprielian@gmail.com>
2020-05-17 02:16:00 +0200

    Update

--- Kit_de_demarrage_Windows_AK.bat
+++ Kit_de_demarrage_Windows_AK.bat
@@ -9,19 +9,15 @@ choco install firefox -fy
 
 :::: Text editors / IDEs
 choco install atom -fy
-choco install sublimetext3 -fy
 choco install visualstudiocode -fy
-choco install visualstudio2015community -fy
+
 
 :::: Dev tools
 choco install git -fy
-choco install nodejs.install -fy
+
 
 :::: Media
 choco install vlc -fy
 
 :::: Utilitiaires
 choco install 7zip.install -fy
-choco install dropbox -fy
-choco install slack -fy
-choco install office365proplus -fy
