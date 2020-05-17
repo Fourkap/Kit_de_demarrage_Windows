e012eb6b10bba16b48a3a54ebb0e835b1100beb4
Alexandre Kaprielian <alex.kaprielian@gmail.com>
2020-05-17 02:58:17 +0200

    Update Kit_de_demarrage_Windows_AK.bat

--- Kit_de_demarrage_Windows_AK.bat
+++ Kit_de_demarrage_Windows_AK.bat
@@ -11,14 +11,30 @@ choco install firefox -fy
 :::: Text editors / IDEs
 choco install atom -fy
 choco install visualstudiocode -fy
+choco install jetbrainstoolbox -fy
+choco install datagrip -fy
+choco install pycharm-edu -fy
+choco install intellijidea-community -fy
+choco install intellijidea-ultimate -fy
+choco install phpstorm- fy
+choco install eclipse-jee-luna -fy
 
 
 :::: Dev tools
 choco install git -fy
+choco install jdk8 -fy
+choco install tomcat -fy
+choco install filezilla -fy
+choco install postman -fy
 
 
 :::: Media
 choco install vlc -fy
+choco install molotov -fy
 
 :::: Utilitiaires
 choco install 7zip.install -fy
+choco install winrar -fy
+choco install hwmonitor -fy
+choco install qbittorrent -fy
+choco install itunes -fy
